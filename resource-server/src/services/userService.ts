9e027161e2cfea7f0435a46b3c2b017e5a44129d
Mammad Yahya <mammadyahyayev@gmail.com>
2024-11-09 17:47:30 +0400

    feat: implement fetching list of users

--- resource-server/src/services/userService.ts
+++ resource-server/src/services/userService.ts
@@ -1,12 +1,13 @@
+const fs = require('fs')
+
 import bcrypt from 'bcrypt';
-import {v4 as uuidv4} from 'uuid';
-import {IUser, User} from "../models/user";
-import {assertNotNull} from "../utils/stringUtils";
-import {UserRequest} from "../models/user";
+import { v4 as uuidv4 } from 'uuid';
+import { IUser, User } from "../models/user";
+import { assertNotNull } from "../utils/stringUtils";
+import { UserRequest } from "../models/user";
+import path from 'path';
 
 const DEFAULT_SALT_ROUND = 12;
-
-const users: IUser [] = []
 
 export interface IUserService {
     createUser: (request: UserRequest, errCallback: (err: any) => void) => Promise<IUser | undefined>
@@ -18,32 +19,47 @@ export const getUserService = (): IUserService => {
 }
 
 class UserService implements IUserService {
-    async createUser(userRequest: UserRequest, errCallback: (text: string) => void): Promise<IUser | undefined> {
-        assertNotNull(userRequest.username, "username")
-        assertNotNull(userRequest.password, "password")
+    usersDataFilePath = path.join(__dirname, '../data/users.json')
+    users: IUser[] = []
 
-        if (this.isUserExistByUsername(userRequest.username)) {
-            errCallback(userRequest.username + " already exists!")
+    async createUser(request: UserRequest, errCallback: (text: string) => void): Promise<IUser | undefined> {
+        assertNotNull(request.username, "username")
+        assertNotNull(request.password, "password")
+
+        if (this.isUserExistByUsername(request.username)) {
+            errCallback(request.username + " already exists!")
             return undefined;
         }
 
         const uuid = uuidv4();
-        const hashedPassword: string = await bcrypt.hash(userRequest.password, DEFAULT_SALT_ROUND);
+        const hashedPassword: string = await bcrypt.hash(request.password, DEFAULT_SALT_ROUND);
         const now = new Date();
-        const user = new User(uuid, userRequest.firstName, userRequest.lastName, userRequest.username, hashedPassword, now.toString(), now.toString());
+        const user = new User(uuid, request.firstName, request.lastName, request.username, hashedPassword, now.toString(), now.toString());
         // todo: save user to db here
         console.log(user.fullName() + "  saved to database");
         user.password = undefined;
-        users.push(user);
+        this.users.push(user);
         return user;
     }
 
     findAllUsers(): IUser[] {
-        return users;
+        if (this.users && this.users.length > 0) {
+            return this.users;
+        }
+
+        fs.readFileSync(this.usersDataFilePath, 'utf8', (err: any, usersData: string) => {
+            if (err) throw err;
+
+            console.log('Reading users data from file: ', this.usersDataFilePath)
+
+            this.users.push(JSON.parse(usersData));
+        })
+
+        return this.users;
     }
 
     private isUserExistByUsername(username: string): boolean {
-        return users.find(user => user.username == username) != undefined;
+        return this.users.find(user => user.username == username) != undefined;
     }
 }
 
